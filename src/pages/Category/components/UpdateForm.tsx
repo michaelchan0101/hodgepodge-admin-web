8cda62169b6207b04ae30dc7bcb461815fad66d4
michael <312073339@qq.com>
2020-07-23 21:14:51 +0800

    feat: update category

--- src/pages/Category/components/UpdateForm.tsx
+++ src/pages/Category/components/UpdateForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal } from 'antd'
+import { Modal, Card, Form, Input, InputNumber, Select } from 'antd'
 
 interface CreateFormProps {
   modalVisible: boolean
@@ -8,9 +8,14 @@ interface CreateFormProps {
   category: Category.Response
 }
 
-const CreateForm: React.FC<CreateFormProps> = props => {
-  const { modalVisible, onCancel } = props
+const formItemLayout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 14 },
+}
 
+const CreateForm: React.FC<CreateFormProps> = props => {
+  const [form] = Form.useForm()
+  const { modalVisible, onCancel, category } = props
   return (
     <Modal
       destroyOnClose
@@ -18,7 +23,24 @@ const CreateForm: React.FC<CreateFormProps> = props => {
       visible={modalVisible}
       onCancel={() => onCancel()}
       footer={null}
-    />
+    >
+      <Card bordered={false} size="small">
+        <Form {...formItemLayout} form={form}>
+          <Form.Item name="name" label="名称" rules={[{ required: true }]}>
+            <Input value={category?.name} />
+          </Form.Item>
+          <Form.Item name="name" label="菜单展示" rules={[{ required: true }]}>
+            <Select defaultValue={category.isShowInMenu ? 1 : 0}>
+              <Select.Option value={1}>是</Select.Option>
+              <Select.Option value={0}>否</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="name" label="排序" rules={[{ required: true }]}>
+            <InputNumber value={category?.sort} />
+          </Form.Item>
+        </Form>
+      </Card>
+    </Modal>
   )
 }
 
