b63aacb9ceb5100730b48d0f93a72c2a3ac86921
michael <312073339@qq.com>
2020-08-26 16:10:29 +0800

    feat: editor style

--- src/pages/Article/create.tsx
+++ src/pages/Article/create.tsx
@@ -1,13 +1,60 @@
-import React from 'react'
+import React, { useState, ChangeEvent } from 'react'
 import { Input, Form } from 'antd'
+import marked from 'marked'
+import { local } from 'webstorage-utils'
+import styles from './style.less'
+
+const CONTENT_CACHE_KEY = 'CREATE_ARTICLE_CONTENT'
+const TITLE_CACHE_KEY = 'CREATE_ARTICLE_TITLE'
 
 const CreateAndUpdateArticle: React.FC<{}> = () => {
   const [form] = Form.useForm()
+  const [content, setContent] = useState(local.get(CONTENT_CACHE_KEY) ?? '')
+  const [title, setTitle] = useState(local.get(TITLE_CACHE_KEY) ?? '')
+  const [contentHtml, setContentHtml] = useState(content ? marked(content) : '')
+  const updateContent = (text: string) => {
+    local.set(CONTENT_CACHE_KEY, text)
+    setContent(text)
+    setContentHtml(marked(text))
+  }
+  const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    updateContent(event.target.value)
+
+    // const now = Date.now()
+    // if (md.updated && now - md.updated > 500) {
+    //   clearTimeout(timer)
+    //   updateContent(event.target.value)
+    // } else {
+    //   clearTimeout(timer)
+    //   timer = setTimeout(updateContent.bind(null, event.target.value), 500)
+    // }
+  }
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    local.set(TITLE_CACHE_KEY, event.target.value)
+    setTitle(event.target.value)
+  }
   return (
     <>
       <Form form={form}>
-        <Input placeholder="请输入标题" size="large" />
-        <Input.TextArea autoSize={{ minRows: 40, maxRows: 40 }} />
+        <Input
+          placeholder="请输入标题......"
+          size="large"
+          className={styles.titleInput}
+          onChange={handleTitleChange}
+          value={title}
+        />
+        <div className={styles.mdContainer}>
+          <Input.TextArea
+            className={styles.editor}
+            autoSize={{ minRows: 50 }}
+            onChange={handleContentChange}
+            value={content}
+          />
+          <div
+            className={styles.review}
+            dangerouslySetInnerHTML={{ __html: contentHtml }}
+          />
+        </div>
       </Form>
     </>
   )
